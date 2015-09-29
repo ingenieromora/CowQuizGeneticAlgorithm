395a09a0a1cfdc8f8828b98ae804e56e7bfd277c
Leandro Mora <leandro.jorge.mora@gmail.com>
2015-09-28 23:53:19 -0300

    Agregada validacion de numeros repetidos

--- ga_quiz_solution.py
+++ ga_quiz_solution.py
@@ -1,5 +1,12 @@
 from pyevolve import G2DList, GSimpleGA;
 
+def getNumberRepeatedValues(matrix, listValues):
+    for row in matrix:
+        for columnValue in row:
+            if columnValue in listValues:
+                listValues.remove(columnValue)
+    return len(listValues);
+
 def eval_func(chromosome):
     score = 0.0
     # iterate over the chromosome
@@ -11,8 +18,19 @@ def eval_func(chromosome):
         if partial_sum == 175:
             score += 10
 
-    if score != 70:
-        score = 0.0
+    cowList = list(xrange(49))
+    repeatedValues = getNumberRepeatedValues(chromosome, cowList)
+
+    if repeatedValues < 30:
+        score +=10
+    elif repeatedValues < 20:
+        score+=20
+    elif repeatedValues < 10:
+        score+=30
+    elif repeatedValues < 40:
+        score+=30
+    elif repeatedValues == 0:
+        score+=1000
 
     return score
 
@@ -20,7 +38,7 @@ genome = G2DList.G2DList(7,7)
 genome.evaluator.set(eval_func)
 genome.setParams(rangemin=1, rangemax=49)
 ga = GSimpleGA.GSimpleGA(genome)
-ga.evolve(freq_stats=10)
+ga.evolve(freq_stats=10000)
 print ga.bestIndividual()
 
 #TODO Hacer que en la representacion no haya numeros repetidos
